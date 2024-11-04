c2e05976b38b145b92a7026ab6b8ceb3540dd124
wtw0212 <wtw0212.hk@gmail.com>
2024-11-04 15:46:46 +0800

    create

--- BMS-system/src/Main.java
+++ BMS-system/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("             Welcome to the Banquet Manage System!");
+        System.out.println("             COMP2411 Database System (2024 Fall)");
+        System.out.println("                       Project Group 43");
+        System.out.println();
+        System.out.println("- [1] Manager Login");
+        System.out.println("- [-1] Exit System");
+        System.out.print(">>> Please select the above options x in [x]:");
     }
 }
