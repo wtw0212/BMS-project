15804c76895bad30e5afa993bcffcdbed3a2a2bf
wtw0212 <wtw0212.hk@gmail.com>
2024-11-06 22:51:05 +0800

    added AdminOptions and UserOptions classes

--- BMS-system/src/AdminOptions.java
+++ BMS-system/src/AdminOptions.java
@@ -40,35 +40,4 @@ public class AdminOptions {
             }
         }
     }
-
-    public void showUserOptions() {
-        boolean loggedIn = true;
-
-        while (loggedIn) {
-            System.out.println("----------------------------------------------------------------");
-            System.out.println("User Options:");
-            System.out.println("- [1] View Available Banquets");
-            System.out.println("- [2] Register for a Banquet");
-            System.out.println("- [-1] Logout");
-            System.out.print(">>> Please select the above options x in [x]: ");
-
-            int option = scanner.nextInt();
-
-            switch (option) {
-                case 1:
-                    new DB_query().viewBanquets(); // Assuming this method shows available banquets
-                    break;
-                case 2:
-                    // Implement banquet registration logic here
-                    System.out.println("Registering for a banquet...");
-                    break;
-                case -1:
-                    System.out.println("Logging out...");
-                    loggedIn = false;
-                    break;
-                default:
-                    System.out.println("Invalid option. Please try again.");
-            }
-        }
-    }
 }
